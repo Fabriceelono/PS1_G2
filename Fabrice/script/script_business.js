5227b54c6138be624231a17565611209e2652909
Fabriceelono <r0912443@student.thomasmore.be>
2023-05-15 21:31:15 +0200

    changes

--- Fabrice/script/script_business.js
+++ Fabrice/script/script_business.js
@@ -39,6 +39,9 @@ function dragDrop() {
         document.getElementById(selectedId).style.display = 'none';
         document.getElementById(dropTargetId).style.display = 'none';
         matchingCounter++;
+    }else {
+        // If the answer is incorrect, show a notification
+        showNotification();
     }
 
     if (matchingCounter === 3) {
@@ -94,4 +97,29 @@ function addEventListeners() {
         item.addEventListener('dragover', dragOver);
         item.addEventListener('dragleave', dragLeave);
     })
+}
+function showNotification() {
+    const notification = document.createElement('div');
+    notification.textContent = 'Incorrect answer. Please try again.';
+    notification.classList.add('notification');
+    notification.style.backgroundColor = 'red';
+    notification.style.color = 'white';
+    notification.style.position = 'fixed';
+    notification.style.top = '0';
+    notification.style.width = '100%';
+    notification.style.padding = '32px';
+    notification.style.textAlign = 'center';
+    notification.style.opacity = '0'; // start with 0 opacity
+    document.body.appendChild(notification);
+
+    // Add a transition effect
+    setTimeout(() => {
+        notification.style.opacity = '1'; // gradually increase opacity
+        notification.style.transition = 'opacity 0.5s ease-in-out'; // add transition effect
+    }, 100);
+    setTimeout(() => {
+        notification.style.opacity = 0;
+        notification.style.pointerEvents = 'none';
+    }, 2000);
+
 }
